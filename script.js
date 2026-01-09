92da9c5a871c37ce2340fe10bd7c8106bbdca75d
Reyansh <reyanshdusanapudi@gmail.com>
2026-01-09 17:24:59 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function showStats(name) {
+  if (name === 'Eddie') {
+    alert(
+      "Eddie's Stats:\n" +
+      "Speed: 100\n" +
+      "Race IQ: 100\n" +
+      "Aggression: 100\n" +
+      "Team: McLaren"
+    );
+  } else if (name === 'Reyansh') {
+    alert(
+      "Reyansh's Stats:\n" +
+      "Speed: 99\n" +
+      "Race IQ: 99\n" +
+      "Consistency: 99\n" +
+      "Team: McLaren"
+    );
+  }
+}
